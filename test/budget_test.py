a1e0433b70ce59908dee1ee7f77185a7fe45a7ad
Raoni Lourenco <raoni@nyu.edu>
2019-11-05 18:36:30 -0500

    quine mmcluskey test

---
+++ test/budget_test.py
@@ -0,0 +1,65 @@
+import os
+import pytest
+import time
+import zmq
+import mldebugger.tree as _tree
+
+from mldebugger.autodebug_trees import AutoDebug
+from mldebugger.quine_mccluskey import reduce_terms
+from mldebugger.run import find_all_paths, from_paths_to_binary
+
+@pytest.mark.incremental
+class TestPythonWorker:
+
+    def kill(self):
+        context = zmq.Context()
+        sender = context.socket(zmq.PUSH)
+        sender.bind("tcp://{0}:{1}".format("*", '5557'))
+        receiver = context.socket(zmq.PULL)
+        receiver.bind("tcp://{0}:{1}".format("*", '5558'))
+        poller = zmq.Poller()
+        poller.register(receiver, zmq.POLLIN)
+
+        sender.send_string('kill')
+
+        poller.unregister(receiver)
+        receiver.close()
+        sender.close()
+        context.term()
+
+    def test_diagnosis_false(self):
+        os.system("python_worker & disown")
+
+        space = {'p0':['a','b','c','d','e','f'],'p1':[0,1,2],'p2':['a','b','c','d','e','f']}
+        filename = 'test/test_diagnosis.vt'
+        outputs = ['result']
+
+        believedecisive, t, total = AutoDebug().run(filename, space, outputs)
+        if _tree.get_depth(t) > 0:
+            keys = space.keys()
+            goodpaths, badpaths, input_dict = find_all_paths(t, keys)
+            minterms, flatten = from_paths_to_binary(badpaths, input_dict)
+            if 0 < len(flatten) < 10:
+                s = reduce_terms(len(flatten), minterms)
+                results = []
+                for prime in s:
+                    result = []
+                    for i in range(len(prime)):
+                        if prime[i] == '1':
+                            comparator = '==' if (
+                                    isinstance(flatten[i][1], str) or isinstance(flatten[i][1], unicode)) else '>='
+                            result.append((keys[flatten[i][0]], comparator, str(flatten[i][1])))
+                        elif prime[i] == '0':
+                            comparator = '!=' if (
+                                    isinstance(flatten[i][1], str) or isinstance(flatten[i][1], unicode)) else '<'
+                            result.append((keys[flatten[i][0]], comparator, str(flatten[i][1])))
+                    results.append(result)
+                assert ('p1','>=','1') in result
+            else:
+                assert False
+        else:
+            assert False
+
+        self.kill()
+
+        assert True
--- test/python_worker_test.py
+++ test/python_worker_test.py
@@ -9,7 +9,7 @@ class TestPythonWorker:
 
 
 
-    def test_diagnosis(self):
+    def test_diagnosis_false(self):
 
         context = zmq.Context()
         sender = context.socket(zmq.PUSH)
@@ -41,3 +41,35 @@ class TestPythonWorker:
         sender.close()
         context.term()
 
+    def test_diagnosis_true(self):
+
+        context = zmq.Context()
+        sender = context.socket(zmq.PUSH)
+        sender.bind("tcp://{0}:{1}".format("*", '5557'))
+        receiver = context.socket(zmq.PULL)
+        receiver.bind("tcp://{0}:{1}".format("*", '5558'))
+        poller = zmq.Poller()
+        poller.register(receiver, zmq.POLLIN)
+
+        os.system("python_worker & disown")
+
+        conf = [0,0,2]
+        inputs = ['p0','p1','p2']
+        filename = 'test/test_diagnosis.vt'
+        outputs = ['result']
+
+        message = '|'.join([str(filename),str(conf),str(inputs),str(outputs)])
+        sender.send_string(message)
+        socks = dict(poller.poll(10000))
+        if socks:
+             if socks.get(receiver) == zmq.POLLIN:
+                 msg = receiver.recv(zmq.NOBLOCK)
+                 assert msg == "[0, 0, 2, 'True']"
+
+        time.sleep(1)
+        sender.send_string('kill')
+
+        poller.unregister(receiver)
+        receiver.close()
+        sender.close()
+        context.term()
